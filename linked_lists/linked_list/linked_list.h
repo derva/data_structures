ac3dbe194514cc30165733a758738bc94675152f
derva <dervishalilcevic@gmail.com>
2021-05-03 06:22:08 +0200

    basic linked_list

--- linked_lists/linked_list/linked_list.h
+++ linked_lists/linked_list/linked_list.h
@@ -1,31 +1,117 @@
 #include <iostream>
 
-template<typename T>
-struct Node{
-public:
-  Node() = default;
-  Node(const T& e) : info_{e} {}
-
-  T info_;
-  Node* next_ = nullptr;
-
-  class iterator{
+template <typename T>
+class List {
   public:
-    iterator() = default;
-    iterator(Node<T>* nodeptr){
-      current = nodeptr;
+  List() = default;
+  List(const List& other){
+    Node* newNode;
+    Node* current;
+    if( first_ != nullptr){
+     destroy(); 
     }
-    iterator operator++(){
+    if( other.first_ == nullptr){
+      first_ = last_ = nullptr;
+      size_ = 0;
+    }
+    else{
+      size_ = other.size_;
+      current = other.first_;
+      first_ = new Node();
+      first_->info_ = current->info_;
+      first_->next_ = nullptr;
+      last_ = first_;
+      current = current->next_;
+      while( current != nullptr){
+        newNode = new Node();
+        newNode->info = current->info_;
+        newNode->next_ = nullptr;
+        last_->next_ = newNode;
+        last_ = newNode;
+        current = current->next_;
+      }
+    }
+  }
+  ~List() {
+    Node* temp;
+    while (first_ != nullptr) {
+      temp = first_;
+      first_ = first_->next_;
+      delete temp;
+    }
+    last_ = nullptr;
+    size_ = 0;
+  }
+  void destroy() {
+    Node* temp;
+    while (first_ != nullptr) {
+      temp = first_;
+      first_ = first_->next_;
+      delete temp;
+    }
+    last_ = nullptr;
+    size_ = 0;
+  }
+  void push_back(const T& element) {
+    if (first_ == nullptr) {
+      first_ = new Node;
+      first_->info_ = element;
+      first_->next_ = nullptr;
+      last_ = first_;
+    } 
+    else {
+      Node* novi = new Node;
+      novi->info_ = element;
+      novi->next_ = nullptr;
+      last_->next_ = novi;
+      last_ = novi;
+    }
+    size_++;
+  }
+  void print() const {
+    Node* temp = first_;
+    while (temp != nullptr) {
+      std::cout << temp->info_ << std::endl;
+      temp = temp->next_;
+    }
+  }
+  size_t size() { return size_; }
+  struct Node {
+public:
+    Node() = default;
+    Node(const T& e) : info_{e} {}
+
+    T info_;
+    Node* next_ = nullptr;
+    size_t size_ = 0;
+  };
+  class iterator {
+public:
+    iterator() : current{nullptr} {}
+    iterator(Node* other) { current = other; }
+    iterator(const iterator& other) { current = other.current; }
+    T& operator*() { return current->info_; }
+    iterator operator++() {
       current = current->next_;
       return *this;
     }
-    T operator*(){
-      return current->info_;
-    }
-    bool operator!=(const Node<T>::iterator& other){
-      return current != other.current;
-    }
-  private:
-    Node<T>* current;
+    bool operator==(const iterator& other) { return current == other.current; }
+    bool operator!=(const iterator& other) { return current != other.current; }
+
+private:
+    Node* current;
   };
+
+  private:
+  Node* first_ = nullptr;
+  Node* last_ = nullptr;
+  size_t size_ = 0;
+  // int capacity = 0;
+public:
+  iterator begin(){
+    return iterator{first_};
+  }
+  iterator end(){
+    return iterator{last_->next_};
+  }
 };
--- linked_lists/linked_list/main.cpp
+++ linked_lists/linked_list/main.cpp
@@ -1,37 +1,17 @@
-#include <iostream>
-#include <vector>
 #include "linked_list.h"
 
 
 int main()
 {
-  Node<int>* newNode, *first, *last;
-  first = nullptr;
-  // head->info_ = 5;
-  // head->next_ = new Node<int>{2};
-  std::cout <<"Enter value: ";
-  int value = 0;
-  std::cin >> value;
-  while ( value != -999) {
-    newNode = new Node<int>{value};
-
-    if( first != nullptr){
-      last->next_ = newNode;
-      last = newNode;
-    }
-    else{
-      first = newNode;
-      last = newNode;
-    }
-
-    std::cout << "Successful!\nEnter new value or -999: ";
-    std::cin >> value;
-  }
-  std::cout << "Linked list members: " << std::endl;
-  Node<int>::iterator lastIt{last->next_};
-  for(Node<int>::iterator ptr{first}; ptr != lastIt; ++ptr){
-    std::cout << *ptr << std::endl; 
-  }
+  List<int> a;  
+  a.push_back(5);
+  a.push_back(25);
+  a.push_back(55);
+  a.push_back(26);
+  a.push_back(0);
+  a.push_back(274);
+  auto e = a.begin();
+  a.print();
   std::cout << "END" << std::endl;
   return 0;
 }
